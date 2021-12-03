bff1a11077592490e9ab6ed33ff1ae5a7225d302
Jimbo Siona <jetigeenn@gmail.com>
2021-12-03 06:12:39 +0300

    init ex01 in d03

---
+++ 0x03/ex01/ScavTrap.cpp
@@ -0,0 +1,5 @@
+//
+// Created by user on 03.12.2021.
+//
+
+#include "ScavTrap.hpp"
---
+++ 0x03/ex01/ScavTrap.hpp
@@ -0,0 +1,19 @@
+//
+// Created by user on 03.12.2021.
+//
+
+#ifndef SCAVTRAP_HPP
+#define SCAVTRAP_HPP
+
+
+class ScavTrap {
+
+public:
+	explicit ScavTrap();
+	~ScavTrap();
+
+	void guardGate();
+};
+
+
+#endif //SCAVTRAP_HPP
